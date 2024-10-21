4c45e496ddb6b8a62f05344f3855f06496d84c74
AdamQureshiGH <122238224+AdamQureshiGH@users.noreply.github.com>
2024-10-20 22:59:18 -0400

    starting Code And Files

---
+++ cipher.h
@@ -0,0 +1,15 @@
+#ifndef CIPHER_H
+#define CIPHER_H
+
+#include <string>
+#include <vector>
+#include <optional>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+class Cipher
+{
+    public:
+    int foo {0};
+};
+#endif
